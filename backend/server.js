e5034eb5740f2ebaae3debbbbb6cbab7e284f72c
Mayank-Tripathi <minkut2005@gmail.com>
2026-07-08 14:26:41 +0530

    3rd

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,11 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
-app.use(cors());
 
 const server = http.createServer(app);
 
 require("dotenv").config();
-
+console.log("CLIENT_URL =", process.env.CLIENT_URL);
 
 app.use(
   cors({
